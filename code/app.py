bd9dec05361cbd553d311aaf145d38768ff7b4d1
Abdelali12-codes <jadelmoulaa3@gmail.com>
2022-07-16 18:47:39 -0400

    Revert "update code/app.py file"
    This reverts commit bddc1d91cb57f63d59880d4de07f4f9ebf23f7e3.

--- code/app.py
+++ code/app.py
@@ -6,10 +6,6 @@ def handler(event, context):
 
     obj = {'firstname':"abdelali", 'lastname':"jadelmoula"}
     print(event)
-    return {
-    'statusCode': 200,
-    'headers': {'Content-Type': 'application/json'},
-    'body': json.dumps(obj)
-       }
+    return  json.dumps(obj)
 
    
