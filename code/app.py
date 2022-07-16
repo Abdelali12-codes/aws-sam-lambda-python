bddc1d91cb57f63d59880d4de07f4f9ebf23f7e3
Abdelali12-codes <jadelmoulaa3@gmail.com>
2022-07-16 18:45:29 -0400

    update code/app.py file

--- code/app.py
+++ code/app.py
@@ -6,6 +6,10 @@ def handler(event, context):
 
     obj = {'firstname':"abdelali", 'lastname':"jadelmoula"}
     print(event)
-    return  json.dumps(obj)
+    return {
+    'statusCode': 200,
+    'headers': {'Content-Type': 'application/json'},
+    'body': json.dumps(obj)
+       }
 
    
