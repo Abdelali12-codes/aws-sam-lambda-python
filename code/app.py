7d0aecfac426b812d36b60639b7284df344628b2
Abdelali12-codes <jadelmoulaa3@gmail.com>
2022-07-16 19:14:11 -0400

    update files

--- code/app.py
+++ code/app.py
@@ -1,11 +1,14 @@
 
 
 import json
+import os
+
 
 def handler(event, context):
 
     obj = {'firstname':"abdelali", 'lastname':"jadelmoula"}
     print(event)
+    print(f"the databasename is {os.environ.get('databaseName')} and dbuser is {os.environ.get('databaseUser')}")
     return  json.dumps(obj)
 
    
